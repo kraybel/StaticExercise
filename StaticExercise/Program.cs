ecaa2c6ce0844a60cb21012a039df81d0b135a6d
Mona Lisa <j.bradley.hanrahan@gmail.com>
2021-02-03 14:55:44 -0600

    Add project files.

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please specify a Fahrenheit temp to convert to Celsius: ");
+            var cel = TempConverter.FahrenheitToCelsius(Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine($"The Celsius temp would be {cel}");
+            Console.WriteLine();
+            Console.Write("Please specify a Celsius temp to convert to Fahrenheit: ");
+            var fahr = TempConverter.CelsiusToFahrenheit(Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine($"The Fahrenheit temp would be {fahr}");
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            return (fahrenheit - 32) / 1.8;
+        }
+
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius * 9) / 5 + 32;
+        }
+    }
+}
